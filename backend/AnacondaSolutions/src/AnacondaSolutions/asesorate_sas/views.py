620cd0f8f3cffa05cc1b1d93c638fb6dce4140f4
felinblackcat <47456740+felinblackcat@users.noreply.github.com>
2019-06-24 19:02:52 -0500

    Update views.py
    creacion del view estudiante.
    A espera del serializados y el modelo

--- backend/AnacondaSolutions/src/AnacondaSolutions/asesorate_sas/views.py
+++ backend/AnacondaSolutions/src/AnacondaSolutions/asesorate_sas/views.py
@@ -2,8 +2,9 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 #importar los modelos creados
-#from servi.express.models import
+#from AnacondaSolutions.asesorate_sas.models import Estudiante
 #importar los serializadores creados :P  
+#EstudianteSerializer
 from AnacondaSolutions.asesorate_sas.serializers import UserSerializer, GroupSerializer
 from rest_framework.response import Response
 
@@ -21,6 +22,14 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer   
+    
+class EstudianteViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Estudiante.objects.all()
+    serializer_class = EstudianteSerializer   
+    
     
     
 

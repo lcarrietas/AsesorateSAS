5abd9d82209df267a2dcb55152ba05eac467fd47
Carlos Bz <cdbzprogr@gmail.com>
2019-06-25 13:11:36 -0500

    Modelo de cotización agregado, este también crea la tabla en la base de datos con el migrate.

--- backend/AnacondaSolutions/src/AnacondaSolutions/asesorate_sas/models.py
+++ backend/AnacondaSolutions/src/AnacondaSolutions/asesorate_sas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+#MODELO COTIZACIÓN
+class Cotizacion(models.Model):
+    id = models.AutoField(primary_key=True)
+    id_estudiante = models.IntegerField(null= False)
+    horas = models.IntegerField(null=False)
+    id_categoria = models.TextField(null=False,
+    choices=([('JR','JUNIOR'),('SB','SENIOR BASIC'),('SM','SENIOR MEDIUM'),
+    ('SA1','SENIOR ADVANCED 1'),('SA2','SENIOR ADVANCED 2')]) )
+
+    #Para consultar el precio, simplemente se llama el metodo get_precio()
+    def get_precio(self):
+        VALOR_HORA = {
+            'JR':33000,
+            'SB':44000,
+            'SM':50000,
+            'SA1':55000,
+            'SA2':60000
+        }
+        return self.horas*VALOR_HORA[self.id_categoria]

31f7ae5e53f991205a54122f35fafc7daf7d91dc
Jean Paul <yjeanpaul@gmail.com>
2019-06-26 13:59:27 -0500

    Se eliminaron archivos de configuracion XAMPP

--- backend/AnacondaSolutions/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\andre\documents\github\asesoratesas\backend\anacondasolutions\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
